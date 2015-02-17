24c708deaf79c7f98d1d9bf8a742e94e99704266
Bryce Bedwell <brycebedwell@gmail.com>
2015-02-17 14:04:21 -0600

    Added gulp build process

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+    , install = require('gulp-install')
+    , args = require('yargs').argv
+    , path = require('path')
+    , dest = args.dest || path.join(__dirname, 'build')
+    ;
+
+gulp.task('build', ['copy', 'install']);
+
+gulp.task('copy', function() {
+    gulp.src([path.join(__dirname + '/**/*.js'), '!./**/gulpfile.js', '!./{jobs,jobs/**/*}', '!./{node_modules,node_modules/**/*}'])
+        .pipe(gulp.dest(dest))
+})
+
+gulp.task('install', function() {
+    return gulp.src('./package.json')
+        .pipe(gulp.dest(dest))
+        .pipe(install({ production:true }));
+})
